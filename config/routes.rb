7fbdeb48df07e4c971ff0ef1d5f01caad2d6d40b
Najma <najmathande@gmail.com>
2023-06-30 23:03:55 +0300

    added models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'signups/create'
+  get 'activities/index'
+  get 'activities/destroy'
+  get 'campers/index'
+  get 'campers/show'
+  get 'campers/create'
+  resources :campers, only: [:index, :show, :create]
+  resources :activities, only: [:index, :destroy]
+  resources :signups, only: [:create]
+
+  get '/campers/:id', to: 'campers#show_with_activities'
+  delete '/activities/:id', to: 'activities#destroy_with_signups'
 end
